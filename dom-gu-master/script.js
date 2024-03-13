cf4b26c687c7f43b7220743c7d1eb2692d71cf0a
brayanc777 <130773223+brayanc777@users.noreply.github.com>
2024-03-13 16:50:28 -0500

    Add files via upload

---
+++ dom-gu-master/script.js
@@ -0,0 +1,66 @@
+let personas = [];
+function capturaDatos() {
+  console.log("Capturando datos...");
+  let persona = {
+    nombre: document.getElementById("nombre").value,
+    correo: document.getElementById("email").value,
+    telefono: document.getElementById("telefono").value,
+    fecha: document.getElementById("fecha").value,
+    ciudad: document.getElementById("ciudad").value,
+  };
+  personas.push(persona);
+  mostrarCards();
+}
+function mostrarCards() {
+  document.getElementById("cards").innerHTML = "";
+  personas.map(function (persona) {
+    let card = document.createElement("section");
+    let nombre = document.createElement("p");
+    let correo = document.createElement("p");
+    let telefono = document.createElement("p");
+    let fecha = document.createElement("p");
+    let ciudad = document.createElement("p");
+    nombre.textContent = persona.nombre;
+    correo.textContent = persona.correo;
+    telefono.textContent = persona.telefono;
+    fecha.textContent = persona.fecha;
+    ciudad.textContent = persona.ciudad;
+    card.append(nombre, correo, telefono, fecha, ciudad);
+    document.getElementById("cards").append(card);
+  });
+}
+
+let inputs = document.querySelectorAll("#formulario input");
+
+inputs.forEach(function (input) {
+  input.addEventListener("click", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+});
+let exprecionesRegulares = {
+ nombre: /^[a-zA-Z]+$/
+}
+
+function validarFormulario(e) {
+  switch (e.target.name) {
+    case "nombre":
+      if(exprecionesRegulares.nombre.test(e.target.value)){
+        console.log('paso')
+      }else{
+        console.log('no paso')
+      }
+      break;
+    case "email":
+      console.log("este es el email...");
+      break;
+    case "telefono":
+      console.log("este es el telefono...");
+      break;
+    case "fecha de nacimiento":
+      console.log("esta es la fecha de nacimiento...");
+      break;
+    case "ciudad":
+      console.log("estse es la ciudad...");
+      break;
+  }
+}
+
